9a5c6cb96fbf5c531763bea4b2054aa83710d611
Tharanga Kasthuriarachchi <gamesfreak26@gmail.com>
2021-05-29 11:06:53 +1000

    Set up port to read from env file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 3000
+require("dotenv").config();  // create a .env file in root folder
+const port = process.env.PORT || 3001
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
