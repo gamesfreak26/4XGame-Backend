9eb7e89021a153ed0dfd4139ea326bb98cee5223
Teej <gamesfreak26@gmail.com>
2021-05-29 10:59:11 +1000

    Inital code

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+})
